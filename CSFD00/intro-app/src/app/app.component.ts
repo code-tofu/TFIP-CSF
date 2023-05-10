16b5bef201925626691ae5dcd758db4fb13b4e83
code-tofu <benjamin.ftc@gmail.com>
2023-05-10 19:13:53 +0800

    Day 3

--- CSFD00/intro-app/src/app/app.component.ts
+++ CSFD00/intro-app/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  // is this an attribute bracket or an array bracket?
+  styles: [`
+  h1{color:red}
+  `]
 })
 export class AppComponent {
-  title = 'intro-app';
+  title = 'Introduction to Angular';
 }
