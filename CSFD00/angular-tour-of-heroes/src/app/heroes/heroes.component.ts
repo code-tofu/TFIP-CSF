4e9cbb3fc9a110a1a68ca582a3765b9b01afae34
code-tofu <benjamin.ftc@gmail.com>
2023-05-15 01:23:11 +0800

    TOH Final

--- CSFD00/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ CSFD00/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,48 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes'; //uppercase because const?
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  // Windstorm implements Hero interface defined by hero.ts
-  heroStarter: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-
-  //   constructor(private heroService: HeroService) {}
-
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
-  //   heroes = HEROES; //from original TS import, changed to service
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  //   getHeroes(): void {
-  //     this.heroes = this.heroService.getHeroes();
-  //   }
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
-  //   onSelect(hero: Hero): void {
-  //     this.selectedHero = hero;
-  //   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 }
