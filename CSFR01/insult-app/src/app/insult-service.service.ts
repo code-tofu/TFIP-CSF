df6e32d00f1bfa06c4dcdda48cbb96eb94a826e4
code_tofu <benjamin.ftc@gmail.com>
2023-05-21 22:46:45 +0800

    21 May

---
+++ CSFR01/insult-app/src/app/insult-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InsultServiceService {
+
+  //  constructor(private http: HttpClient) {}
+
+  
+
+  
+
+}
---
+++ CSFR01/insult-app/src/app/insult.ts
@@ -0,0 +1,5 @@
+export interface Insult {
+    "to": string;
+    "from": string;
+    "insultsList": string[];
+}
